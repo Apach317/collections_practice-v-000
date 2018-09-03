9219e2b05589c09356a7f2f0cd91ac2e1feac867
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 22:58:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,16 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort
+  array.length do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
+end
+array.sort
 end
 
 def swap_elements
